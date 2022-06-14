996218729fc5ee594e55beaf2f1dd9aaeb9a537f
Jano <martinojano0@gmail.com>
2022-06-14 13:41:50 -0300

    add gitignore, add folder formulario

--- uploader/uploader.js
+++ uploader/uploader.js
@@ -3,27 +3,29 @@ const $main = d.querySelector("main");
 const $files = d.getElementById("files");
 
 const uploader = (file) => {
-  fetch("assets/uploader.html", {
-    method: "POST",
-    headers: {
-      "Content-Type": "multipart/form-data",
-    },
-  })
-    .then(() => {
-      //   let xhr = new XMLHttpRequest();
-      //   let formData = new FormData();
-      //   formData.append("file", file);
-      //   xhr.send(formData);
-    })
-    .catch((err) => {
-      console.error(err);
-      $main.insertAdjacentHTML("afterend", `<p><b>${err}</b></p>`);
-    });
+  const xhr = new XMLHttpRequest(),
+    formData = new FormData();
+
+  formData.append("file", file);
+  d.addEventListener("readystatechange", (e) => {
+    if (xhr.readyState !== 4) return;
+
+    if (xhr.status >= 200 && xhr.status < 300) {
+      let json = JSON.parse(xhr.responseText);
+      console.log(json);
+    } else {
+      console.error(`Error ${xhr.status}: ${xhr}`);
+    }
+  });
+
+  xhr.open("POST", "uploader.php");
+  xhr.setRequestHeader("enc-type", "multipart/form-data");
+  xhr.send(formData);
 };
 
 d.addEventListener("change", (e) => {
   if (e.target === $files) {
-    let files = Array.from(e.target.files);
+    const files = Array.from(e.target.files);
     files.forEach((el) => {
       uploader(el);
     });
