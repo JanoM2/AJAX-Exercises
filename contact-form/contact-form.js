5246c2afe5cf0fc56fe33649bf80a4ca3fb6f9f2
Jano <martinojano0@gmail.com>
2022-06-27 16:19:00 -0300

    add contact-form folder

---
+++ contact-form/contact-form.js
@@ -0,0 +1,66 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const d = document;
+    const $form = d.querySelector(".contact-form");
+    let $input = d.querySelectorAll(".contact-form [data-form]");
+  
+    $input.forEach((input) => {
+      const $span = d.createElement("span");
+      $span.id = input.className;
+      $span.textContent = input.title;
+      $span.classList.add("contact-form-error", "none");
+      input.insertAdjacentElement("afterend", $span);
+    });
+  
+    d.addEventListener("keyup", (e) => {
+      if (e.target.matches(".contact-form [required]")) {
+        let $input = e.target;
+        let pattern = $input.pattern || $input.dataset.pattern;
+  
+        if (pattern && $input.value !== "") {
+          let regex = new RegExp(pattern);
+          return !regex.exec($input.value)
+            ? d.getElementById($input.classList).classList.add("is-active")
+            : d.getElementById($input.classList).classList.remove("is-active");
+        }
+  
+        if (!pattern) {
+          return $input.value === ""
+            ? d.getElementById($input.classList).classList.add("is-active")
+            : d.getElementById($input.classList).classList.remove("is-active");
+        }
+      }
+    });
+  
+    $form.addEventListener("submit", (e) => {
+      e.preventDefault();
+  
+      fetch("send_mail.php", {
+        method: "POST",
+        body: new FormData($form),
+      })
+        .then((res) => {
+          return res.json();
+        })
+        .then((json) => {
+          console.log(json);
+          const $response = d.querySelector(".contact-form-response");
+          const $loader = d.querySelector(".lds-ring");
+          $loader.classList.remove("none");
+  
+          setTimeout(() => {
+            $loader.classList.add("none");
+            $response.classList.remove("none");
+            $form.reset();
+  
+            setTimeout(() => {
+              $response.classList.add("none");
+            }, 3000);
+          }, 3000);
+        })
+        .catch((err) => {
+          console.error(err);
+          $form.insertAdjacentHTML("afterend", `<p class="error">${err}</p>`);
+        });
+    });
+  });
+  
---
+++ contact-form/send_mail.php
@@ -0,0 +1,40 @@
+<?php
+echo $_SERVER["HTTP_HOST"];
+if(isset($_POST)) {
+    error_reporting(0);
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $comments = $_POST["comment"];
+    $domain = $_SERVER["HTTP_HOST"];
+    $to = "martinojano0@gmail.com";
+    $subject = "Contacto desde el formulario del sitio $domain: $subject";
+    $message = "
+    <p>
+    Datos enviados desde el formulario del sitio <b>$domain</b>
+    </p>
+    <ul>
+    <li>Nombre: $name</li>
+    <li>Email: $email</li>
+    <li>Asunto: $subject</li>
+    <li>Comentarios: $comments</li>
+    </ul>
+    ";
+    $headers = "MIME-version1.0\r\n"."Content-Type: text/html; charset=utf-8\r\n".
+    "From: Envio Automatico No Responder <no-reply@$domain>";
+
+    $send_mail = mail($to,$subject,$message,$headers);
+    
+    if($send_mail){
+        $res = ["err"=> false,
+        "message"=> "Tus datos han sido enviados"];
+    }else{
+        $res = ["err"=> true,
+        "message"=> "Error al enviar tus datos, intenta nuevamente"];
+    };
+    
+header("Content-type: application/json");
+echo json_encode($res);
+exit;
+
+}
